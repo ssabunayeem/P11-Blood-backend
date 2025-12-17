f7ce7adc3a1bdef86cb2b94f19e81a60d5c510a3
ssabunayeem <ssabunayeem@gmail.com>
2025-12-17 21:17:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//  mongoDB code
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://p11-blood:qlVhNI4Emh7u1lZf@cluster0.atr0nay.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+
+
+
+
+
+
+
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello, Mission SCIC ");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
